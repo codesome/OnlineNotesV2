67af5074c783da5882086a63f04e7199dbc4fd8e
Ganesh <ganeshdv.17@gmail.com>
2016-02-08 17:20:10 +0530

    signup login _and notes for each _and edit,delete note done

--- public/js/jquery-profile.js
+++ public/js/jquery-profile.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
    $("#create").click(function(){
        $("#new-note").css({"display":"block" , "transition":"display 1s"});
    });
+    
+    $("#edit").click(function(){
+        $("#aNote").slideUp();
+        $("#edit-form").slideDown();
+    });
+    
+    $("#cancel-edit").click(function(){
+        window.location.reload(true);
+    });
 });
--- routes/profile.js
+++ routes/profile.js
@@ -80,13 +80,23 @@ router.get('/readnote/:id' , function(req,res,next){
     }
     else{
         var userid = obj.siStatus(req,obj,keys.cookieKey);
+        var noteid = req.params.id;
         con.query('select userkey from users where id=?' , [userid] ,function(err,row){
             var key = row[0].userkey;
             con.query(
                 "select * from "+userid+" where id=?",
-                [req.params.id],
+                [noteid],
                 function(err,rows){
-                    res.render('read',{name:obj.decrypt(rows[0].name,key),content:obj.decrypt(rows[0].content,key)});
+                    if(rows.length){
+                        res.render('read',{
+                            name : obj.decrypt(rows[0].name,key),
+                            content : obj.decrypt(rows[0].content,key),
+                            id : noteid
+                        });
+                    }
+                    else{
+                        res.send('does not exist');
+                    }
                 });
         });
         /*con.query(
@@ -99,28 +109,55 @@ router.get('/readnote/:id' , function(req,res,next){
     
 });
 
+router.get('/deletenote/:id' , function(req,res,next){
+    if(!obj.siStatus(req,obj,keys.cookieKey)){
+        res.redirect('/');
+    }
+    else{
+        var userid = obj.siStatus(req,obj,keys.cookieKey);
+        
+        con.query(
+            'delete from '+userid+' where id=?', 
+            [req.params.id] , 
+            function(err){
+                if(err) throw err;
+                res.redirect('/profile/list')
+            });
+    }
+});
+
+
 router.get('/logout' , function(req,res,next){
     res.clearCookie('user');
     res.redirect('/');
 })
 
-/*router.get('/notelist' , function(req,res,next){
+router.post('/edit/:id' , function(req,res,next){
     if(!obj.siStatus(req,obj,keys.cookieKey)){
-        res.redirect('/')
+        res.redirect('/');
     }
     else{
-        con.query(
-        "select * from notes" , 
-        function(err,rows){
-            console.log(rows);
-            var i , content = '';
-            for(i=0;i<rows.length;i++){
-                content+='<br>'+rows[i].name+'&nbsp; <a href="/profile/readnote/'+rows[i].id+'">read</a>';
-            }
-            res.send(content);
+        var userid = obj.siStatus(req,obj,keys.cookieKey);
+        var noteid = req.params.id;
+        con.query('select userkey from users where id=?' , [userid] ,function(err,row){
+            var key = row[0].userkey;
+            var name = obj.encrypt(req.body.editedName,key);
+            var content = obj.encrypt(req.body.editedContent,key);
+            
+            con.query("update "+userid+" set name=?,content=? where id=?",
+                     [name,content,noteid],
+                      function(err){if(err) throw err;}
+                     );
+            
+            con.query(
+                "select * from "+userid+" where id=?",
+                [noteid],
+                function(err,rows){
+                        res.redirect('/profile/readnote/'+noteid);
+                    });
         });
     }
     
-});*/
+});
 
 module.exports = router;
